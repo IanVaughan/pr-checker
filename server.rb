aa5513c3408d73a27d866bc5c8fa8238ea5d7f5f
Ian Vaughan <github@ianvaughan.co.uk>
2016-02-10 23:51:24 +0000

    Update to use env

--- server.rb
+++ server.rb
@@ -1,47 +1,73 @@
 require 'sinatra'
 require 'json'
 require 'octokit'
+require 'dotenv'
+require 'pry'
 
-REPO = "QuiqUpLTD/QuiqupAPI"
-PLUS_ONE_TEXT = /:\+1:/
-OK_LABEL = '+2d'
+Dotenv.load
+PLUS_ONE_TEXT = ENV["PR_CHECKER_PLUS_ONE_TEXT"]
+PLUS_ONE_TEXT_REGEXP = Regexp.quote PLUS_ONE_TEXT
+OK_LABEL = ENV["PR_CHECKER_OK_LABEL"]
+ACCESS_TOKEN = ENV["PR_CHECKER_ACCESS_TOKEN"]
+CONTEXT = ENV["PR_CHECKER_CONTEXT"]
+INFO = ENV["PR_CHECKER_INFO"]
 
-client = Octokit::Client.new(:access_token => ENV.fetch("ACCESS_TOKEN"))
-client.user # assets access is working otherwise will raise
+puts "Read from env:"
+puts "PLUS_ONE_TEXT: #{PLUS_ONE_TEXT}"
+puts "OK_LABEL: #{OK_LABEL}"
+puts "ACCESS_TOKEN: #{ACCESS_TOKEN}"
+puts "CONTEXT: #{CONTEXT}"
+puts "INFO: #{INFO}"
 
-# Required for Docker
-set :bind, '0.0.0.0'
+client = Octokit::Client.new(access_token: ACCESS_TOKEN)
+begin
+  client.user # assert access is working otherwise will raise
+rescue Octokit::Unauthorized
+  puts "ACCESS_TOKEN does not work! I don't have access. #{ACCESS_TOKEN}"
+  exit
+end
+
+set :bind, '0.0.0.0' # Required for Docker
 
 get "/ping" do
   "pong"
 end
 
+get "/github" do
+  # refresh access
+end
+
 post '/payload' do
   push = JSON.parse(request.body.read)
+  puts "payload:#{push}"
   return unless push.key?("issue")
   return unless push["issue"].key?("number")
   issue_number = push["issue"]["number"]
+  repo = push["repository"]["full_name"]
+  puts "repo:#{repo}, issue_number:#{issue_number}"
 
-  comments = client.issue_comments(REPO, issue_number)
-  plus_one_count = comments.count { |c| c[:body].match PLUS_ONE_TEXT }
+  begin
+    comments = client.issue_comments(repo, issue_number)
+  rescue Octokit::NotFound => e
+    puts "ERROR: cannot find issue. #{e}"
+    return
+  end
 
-  puts "Received #{issue_number}:#{plus_one_count}"
+  plus_one_count = comments.count { |c| c[:body].match PLUS_ONE_TEXT_REGEXP }
 
-  commits = client.pull_commits REPO, issue_number
+  puts "repo:#{repo}, issue_number:#{issue_number}, count:#{plus_one_count}"
+
+  commits = client.pull_commits repo, issue_number
   commit_sha = commits.last[:sha]
-  info = {
-    context: "2+1s",
-    infomation: "You have 2+1s and are go for merge!"
-  }
-
-  puts commit_sha
-  puts info
+  info = { context: CONTEXT, infomation: INFO }
+  puts "commit:#{commit_sha}, info:#{info}"
 
   if plus_one_count > 1
-    client.add_labels_to_an_issue(REPO, issue_number, [OK_LABEL]) 
-
-    client.create_status(REPO, commit_sha, 'success', info)
+    client.add_labels_to_an_issue(repo, issue_number, [OK_LABEL])
+    client.create_status(repo, commit_sha, 'success', info)
   else
-    client.create_status(REPO, commit_sha, 'pending', info)
+    client.remove_label(repo, issue_number, OK_LABEL)
+    client.create_status(repo, commit_sha, 'pending', info)
   end
+  puts "end..."
 end
