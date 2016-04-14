e14962de1daefac5bf86b68c0b845c52ec541e02
Ian Vaughan <github@ianvaughan.co.uk>
2016-04-14 14:13:03 +0100

    Tidy deploy script and follow log once started

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
+echo "* Building..."
 docker build --tag pr-checker-image .
+echo "* Stopping current instance..."
 docker stop pr-checker-container
+echo "* Removing old instance..."
 docker rm pr-checker-container
+echo "* Starting new instance..."
 docker run -d --env-file .env -p 4444:4567 --name pr-checker-container pr-checker-image
+echo "* Showing you the logs..."
+docker logs -f pr-checker-container
