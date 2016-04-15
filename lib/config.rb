06d406cb00f5556a55a1a2f9c06856f08fea0c25
Ian Vaughan <github@ianvaughan.co.uk>
2016-04-15 01:27:59 +0100

    Accept either text or emoji plus 1

--- lib/config.rb
+++ lib/config.rb
@@ -1,17 +1,25 @@
+# require "dotenv"
+
 module PrChecker
   class Config
-    attr_reader :plus_one_text, :ok_label, :access_token, :context, :info
+    attr_reader :plus_one_text, :plus_one_text_regexp, :plus_one_emoji_regexp
+    attr_reader :ok_label, :access_token, :context, :info
 
     def initialize
       load_env
     end
 
     def load_env
-      @plus_one_text = ENV["PR_CHECKER_PLUS_ONE_TEXT"]
-      @ok_label = ENV["PR_CHECKER_OK_LABEL"]
-      @access_token = ENV["PR_CHECKER_ACCESS_TOKEN"]
-      @context = ENV["PR_CHECKER_CONTEXT"]
-      @info = ENV["PR_CHECKER_INFO"]
+      # Dotenv.load
+      # GITHUB_APP_TOKEN
+      @plus_one_text = ENV.fetch("PR_CHECKER_PLUS_ONE_TEXT", ":+1:")
+      @plus_one_text_regexp = Regexp.quote @plus_one_text
+      # @plus_one_emoji_regexp = Regexp.quote ENV.fetch("PR_CHECKER_PLUS_ONE_EMOJI")
+      @plus_one_emoji_regexp = ENV.fetch("PR_CHECKER_PLUS_ONE_EMOJI")
+      @ok_label = ENV.fetch("PR_CHECKER_OK_LABEL", "+2d")
+      @access_token = ENV.fetch("PR_CHECKER_ACCESS_TOKEN")
+      @context = ENV.fetch("PR_CHECKER_CONTEXT", "No context configured")
+      @info = ENV.fetch("PR_CHECKER_INFO", "No info configured")
 
       puts "Read from env:"
       puts "PLUS_ONE_TEXT: #{@plus_one_text}"
--- lib/parser.rb
+++ lib/parser.rb
@@ -1,3 +1,4 @@
+require "logger"
 require 'json'
 
 module PrChecker
@@ -6,40 +7,41 @@ module PrChecker
 
     def initialize(config, client)
       @config, @client = config, client
+      # @log = File.new("log/sinatra.log", "a+")
+      @logger = Logger.new(STDERR)
     end
 
     def parse(request)
-      push = JSON.parse(request.body.read)
-      puts "payload:#{push}"
-      return unless push.key?("issue")
-      return unless push["issue"].key?("number")
-      issue_number = push["issue"]["number"]
-      repo = push["repository"]["full_name"]
-      puts "repo:#{repo}, issue_number:#{issue_number}"
+      data = JSON.parse(request.body.read)
+      return "No issue found in payload" unless data.key?("issue")
+      return "No number found in payload" unless data["issue"].key?("number")
+
+      issue_number = data["issue"]["number"]
+      org_repo = data["repository"]["full_name"]
 
       begin
-        comments = client.issue_comments(repo, issue_number)
+        comments = client.issue_comments(org_repo, issue_number)
       rescue Octokit::NotFound => e
         puts "ERROR: cannot find issue. #{e}"
-        return
+        return "Failed to get comments"
       end
 
-      plus_one_count = comments.count { |c| c[:body].match config.plus_one_text }
+      @logger.warn "comments: #{comments.map { |c| c[:body] }}"
 
-      puts "repo:#{repo}, issue_number:#{issue_number}, count:#{plus_one_count}"
+      plus_one_count = comments.count { |c| c[:body].match config.plus_one_text_regexp }
+      plus_one_count += comments.count { |c| c[:body].match config.plus_one_emoji_regexp }
 
-      commits = client.pull_commits repo, issue_number
+      commits = client.pull_commits org_repo, issue_number
       commit_sha = commits.last[:sha]
       info = { context: config.context, infomation: config.info }
-      puts "commit:#{commit_sha}, info:#{info}"
 
       if plus_one_count > 1
-        client.add_labels_to_an_issue(repo, issue_number, [config.ok_label])
-        client.create_status(repo, commit_sha, 'success', info)
+        client.add_labels_to_an_issue(org_repo, issue_number, [config.ok_label])
+        client.create_status(org_repo, commit_sha, 'success', info)
       else
-        client.create_status(repo, commit_sha, 'pending', info)
+        client.create_status(org_repo, commit_sha, 'pending', info)
       end
-      puts "end..."
+      plus_one_count
     end
   end
 end
