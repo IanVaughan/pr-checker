00db2f415ca7f89efaf4fece7a6f6bd9df067b41
Ian Vaughan <github@ianvaughan.co.uk>
2016-04-21 15:50:11 +0100

    Add dotenv back

--- lib/config.rb
+++ lib/config.rb
@@ -1,4 +1,4 @@
-# require "dotenv"
+require "dotenv"
 
 module PrChecker
   class Config
@@ -10,7 +10,7 @@ module PrChecker
     end
 
     def load_env
-      # Dotenv.load
+      Dotenv.load
       # GITHUB_APP_TOKEN
       @plus_one_text = ENV.fetch("PR_CHECKER_PLUS_ONE_TEXT", ":+1:")
       @plus_one_text_regexp = Regexp.quote @plus_one_text
