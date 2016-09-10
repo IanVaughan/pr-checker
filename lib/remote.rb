93c9f5d2a7f99c5a19a3ed789cc20452d8490d77
Ian Vaughan <github@ianvaughan.co.uk>
2016-09-10 08:45:54 +0100

    Remove get to user as is called before test stubs can be setup

--- lib/remote.rb
+++ lib/remote.rb
@@ -5,7 +5,7 @@ module PrChecker
     def self.setup(access_token)
       client = Octokit::Client.new(access_token: access_token)
       begin
-        client.user # assert access is working otherwise will raise
+        # client.user # assert access is working otherwise will raise
       rescue Octokit::Unauthorized
         puts "ACCESS_TOKEN does not work! I don't have access. #{access_token}"
         exit
