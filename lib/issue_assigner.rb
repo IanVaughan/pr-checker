6e685bed3b3c2097c4b60ba5ebd9dab4074c9ec4
Ian Vaughan <github@ianvaughan.co.uk>
2016-09-13 23:07:58 +0100

    Update IssueAssigner to work with config

--- lib/issue_assigner.rb
+++ lib/issue_assigner.rb
@@ -1,15 +1,12 @@
 class IssueAssigner
-
   def initialize(client, config)
     @client = client
     @config = config
   end
 
   def call(org_repo, issue_number)
-    assignees = get_names_from(files)
-
-    assign(org_repo, issue_number, assignees)
-    "Assigned:#{assignees}"
+    assign(org_repo, issue_number, config.assignees)
+    "Assigned:#{config.assignees}"
   end
 
   def unassign(org_repo, issue_number, assignees)
@@ -19,12 +16,6 @@ class IssueAssigner
   private
 
   attr_reader :client, :config
-
-  def get_names_from(files)
-    files.flat_map do |file|
-      Base64.decode64(file[:content]).split
-    end.uniq
-  end
 
   def assign(org_repo, issue_number, assignees)
     client.post "/repos/#{org_repo}/issues/#{issue_number}/assignees", { assignees: assignees }
--- spec/issue_assigner_spec.rb
+++ spec/issue_assigner_spec.rb
@@ -4,72 +4,23 @@ require 'json'
 RSpec.describe IssueAssigner do
   let(:instance) { described_class.new(client, config) }
   let(:client) { double PrChecker::Remote }
-  let(:config) { double 'config', assignees_filenames: assignees_filenames }
+  let(:config) { double 'config', assignees: assignees }
+  let(:assignees) do
+    [ 'Paul', 'Foo' ]
+  end
 
   let(:org_repo) { 'OrgName/RepoName' }
   let(:issue_number) { 42 }
 
-  context 'finds no file' do
-    let(:assignees_filenames) { ['not-existent-file'] }
+  let(:returned_result) { instance.call(org_repo, issue_number) }
 
-    it 'does not update assignees' do
-      expect(client).to receive(:get).with(
-        "/repos/#{org_repo}/contents/#{assignees_filenames.last}"
-      ).and_raise(Octokit::NotFound)
+  it 'assigns the names from the file to the pull request' do
+    expect(client).to receive(:post).with(
+      "/repos/#{org_repo}/issues/#{issue_number}/assignees",
+      { assignees: assignees }
+    )
 
-      expect(client).to_not receive(:post)
-
-      returned_result = instance.call(org_repo, issue_number)
-      expect(returned_result).to eq "Could not find files:#{assignees_filenames}"
-    end
-  end
-
-  context 'finds one of the assignee files' do
-    let(:get_contents) { load_fixture('get_contents') }
-    let(:assignees_filenames) { ['pull_request_assignees', 'not-existent-file'] }
-    let(:assignees) { ["OnOneLine", "TwoNames", "OnSameLine"] }
-
-    it 'assigns the names from the file to the pull request' do
-      expect(client).to receive(:get).with(
-        "/repos/#{org_repo}/contents/#{assignees_filenames.first}"
-      ).and_return(get_contents)
-
-      expect(client).to receive(:get).with(
-        "/repos/#{org_repo}/contents/#{assignees_filenames.last}"
-      ).and_raise(Octokit::NotFound)
-
-      expect(client).to receive(:post).with(
-        "/repos/#{org_repo}/issues/#{issue_number}/assignees",
-        { assignees: assignees }
-      )
-
-      returned_result = instance.call(org_repo, issue_number)
-      expect(returned_result).to eq "Assigned:#{assignees}, from files:#{assignees_filenames}"
-    end
-  end
-
-  context 'finds two assignee files' do
-    let(:get_contents) { load_fixture('get_contents') }
-    let(:get_contents2) { get_contents.merge content: Base64.encode64("OnOneLine AnewName") }
-    let(:assignees_filenames) { ['pull_request_assignees', 'another_file'] }
-    let(:assignees) { ["OnOneLine", "TwoNames", "OnSameLine", 'AnewName'] }
-
-    it 'assigns unique names from both files' do
-      expect(client).to receive(:get).with(
-        "/repos/#{org_repo}/contents/#{assignees_filenames.first}"
-      ).and_return(get_contents)
-
-      expect(client).to receive(:get).with(
-        "/repos/#{org_repo}/contents/#{assignees_filenames.last}"
-      ).and_return(get_contents2)
-
-      expect(client).to receive(:post).with(
-        "/repos/#{org_repo}/issues/#{issue_number}/assignees",
-        { assignees: assignees }
-      )
-
-      returned_result = instance.call(org_repo, issue_number)
-      expect(returned_result).to eq "Assigned:#{assignees}, from files:#{assignees_filenames}"
-    end
+    expect(returned_result).to \
+      eq "Assigned:#{assignees}"
   end
 end
