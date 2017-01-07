67d1411923328673431da592c62b08a9b3edd2d6
Ian Vaughan <github@ianvaughan.co.uk>
2017-01-07 09:10:08 +0000

    Tidy deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,12 +3,17 @@ set +e
 
 MACHINE=quiqup
 IMAGE=ianvaughan/pr-checker
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+echo "This branch:$BRANCH"
 
 docker-machine start $MACHINE
+docker-machine env $MACHINE
 eval $(docker-machine env $MACHINE)
 
 echo "* Building..."
-docker build --tag $IMAGE .
+docker build --tag $IMAGE:$BRANCH .
 
 echo "* Pushing..."
-docker push $IMAGE
+docker push $IMAGE:$BRANCH
+echo "Pushed $IMAGE:$BRANCH"
