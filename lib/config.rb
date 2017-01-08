02033a7385ba854b1d9909ddba679a159ee57610
Ian Vaughan <github@ianvaughan.co.uk>
2017-01-08 17:09:41 +0000

    Remove PrChecker module

--- lib/config.rb
+++ lib/config.rb
@@ -1,17 +1,15 @@
 # envconfig
 require "dotenv"
 
-module PrChecker
-  class Config
-    attr_reader :access_token
+class Config
+  attr_reader :access_token
 
-    def initialize
-      load_env
-    end
+  def initialize
+    load_env
+  end
 
-    def load_env
-      Dotenv.load
-      @access_token = ENV.fetch("PR_CHECKER_ACCESS_TOKEN")
-    end
+  def load_env
+    Dotenv.load
+    @access_token = ENV.fetch("PR_CHECKER_ACCESS_TOKEN")
   end
 end
--- lib/unassign_reviewers.rb
+++ lib/unassign_reviewers.rb
@@ -1,32 +1,30 @@
-module PrChecker
-  class UnassignReviewers
-    def initialize(logger, issue_assigner)
-      @logger = logger
-      @issue_assigner = issue_assigner
-    end
+class UnassignReviewers
+  def initialize(logger, issue_assigner)
+    @logger = logger
+    @issue_assigner = issue_assigner
+  end
 
-    def call(comments, org_repo, issue_number, config)
-      comments.map do |sawer_comment|
-        comment = sawer_comment.to_hash
-        if any_match?(comment, config)
-          user = extract_user_from(comment)
-          next if user.nil?
-          logger.debug "#{org_repo}:#{issue_number} removing assignee #{user}"
-          issue_assigner.unassign(org_repo, issue_number, user)
-        end
+  def call(comments, org_repo, issue_number, config)
+    comments.map do |sawer_comment|
+      comment = sawer_comment.to_hash
+      if any_match?(comment, config)
+        user = extract_user_from(comment)
+        next if user.nil?
+        logger.debug "#{org_repo}:#{issue_number} removing assignee #{user}"
+        issue_assigner.unassign(org_repo, issue_number, user)
       end
     end
+  end
 
-    private
+  private
 
-    attr_reader :issue_assigner, :logger
+  attr_reader :issue_assigner, :logger
 
-    def any_match?(comment, config)
-      config[:review_matches].any? { |plus_one| comment[:body].match(plus_one) }
-    end
+  def any_match?(comment, config)
+    config[:review_matches].any? { |plus_one| comment[:body].match(plus_one) }
+  end
 
-    def extract_user_from(comment)
-      comment.fetch(:user, {}).fetch(:login, nil)
-    end
+  def extract_user_from(comment)
+    comment.fetch(:user, {}).fetch(:login, nil)
   end
 end
