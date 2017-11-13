320717812d47e6d766e2ad9a802adcbcb6176141
Ian Vaughan <ian@quiqup.com>
2017-11-13 23:02:56 +0000

    give reason and don't delete

--- lib/workers/branch.rb
+++ lib/workers/branch.rb
@@ -9,13 +9,13 @@ module Workers
       project = ::Project.find(project_id)
       branch = project.branches.find_by(name: branch_name)
 
-      delete_branch(project_id, branch) if branch.merged?
-      # delete_branch(project_id, branch) if branch.commit["created_at"] < 1.year.ago
+      delete_branch(project_id, branch, "merged") if branch.merged?
+      delete_branch(project_id, branch, "1 year old") if branch.commit["created_at"] < 1.year.ago
     end
 
-    def delete_branch(project_id, branch)
-      logger.info "Workers::Branch deleting branch project_id:#{project_id}, branch:#{branch.name}"
-      Gitlab.delete_branch(project_id, branch.name)
+    def delete_branch(project_id, branch, reason)
+      logger.info "Workers::Branch deleting branch project_id:#{project_id}, branch:#{branch.name}, reason:#{reason}"
+      # Gitlab.delete_branch(project_id, branch.name)
     end
   end
 end
